08719041bf1fe804a5a22586f584549b3531fa6e
teradaian <teradaian@gmail.com>
2021-08-23 21:39:52 -0700

    init styles navbar

--- src/Components/Nav/Nav.jsx
+++ src/Components/Nav/Nav.jsx
@@ -1,26 +1,48 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import * as navStyles from './Nav.module.css'
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import * as navStyles from './Nav.module.css';
 
 function Nav({ displayForm, handleLogout, loggedIn }) {
   
   const loggedOutNav = (
-    <header className={navStyles.header}>
-      <ul>
-        <li onClick={() => displayForm('login')}>Login</li>
-        <li onClick={() => displayForm('signup')}>Signup</li>
-      </ul>
-    </header>
+      <>
+        <Button onClick={() => displayForm('login')}>Login</Button>
+        <Button onClick={() => displayForm('signup')}>Signup</Button>
+      </>
   );
 
   const loggedInNav = (
-    <header className={navStyles.header}>
-      <ul>
-        <li onClick={handleLogout}>Logout</li>
-      </ul>
-    </header>
+      <>
+        <Button onClick={handleLogout}>Logout</Button>
+      </>
   );
-  return <div>{loggedIn ? loggedInNav : loggedOutNav} </div>;
+
+  return (
+    <>
+    <header className={navStyles.header}>
+      <AppBar position="static" >
+        <Toolbar className={navStyles.toolBar}>
+        <div className={navStyles.leftMenu}>
+          <IconButton edge="start" className={navStyles.menuButton} color="inherit" aria-label="menu">
+          </IconButton>
+          <Typography variant="h6" className={navStyles.title}>
+            <img className={navStyles.logo} src="/Assets/images/bloom-logo.png" alt="bloom logo"/>
+          </Typography>
+          </div>
+          <div className={navStyles.rightMenu}>
+            {loggedIn ? loggedInNav : loggedOutNav} 
+          </div>
+        </Toolbar>
+      </AppBar>
+      </header>
+    </>
+    );
 }
 
 export default Nav;
