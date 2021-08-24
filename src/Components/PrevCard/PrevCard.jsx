858baae5a345a06cd97b7537b773598c817372c9
teradaian <teradaian@gmail.com>
2021-08-24 16:45:47 -0700

    add icons to opp view

--- src/Components/PrevCard/PrevCard.jsx
+++ src/Components/PrevCard/PrevCard.jsx
@@ -5,9 +5,12 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
+import Box from '@material-ui/core/Box';
 import Typography from '@material-ui/core/Typography';
 import { Link } from 'react-router-dom'
 import classes from './PrevCard.module.css'
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import VisibilityIcon from '@material-ui/icons/Visibility';
 
 export default function MediaCard({ name, date, notes, id }) {
   
@@ -23,26 +26,34 @@ export default function MediaCard({ name, date, notes, id }) {
             title="placeholder"
           />
           <CardContent>
-            <Typography gutterBottom variant="h5" component="h2">
+            <Typography gutterBottom variant="h5" component="h2" noWrap='false'>
               {name}
             </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
+            <Typography variant="body2" color="textSecondary" component="p" noWrap='false'>
               {date} 
               {notes}
             </Typography>
           </CardContent>
         </CardActionArea>
         <CardActions>
-          <Link to={`/opportunities/${id}`}>
-            <Button size="small" color="primary">
+            <Button 
+            component={Link}
+            to={`/opportunities/${id}`}
+            size="small" 
+            color="primary"
+            startIcon={<VisibilityIcon />}
+            >
                 View
+            </Button>     
+            <Button 
+            component={Link}
+            to={`/opportunities/${id}/new-activity`}
+            size="small" 
+            color="primary"
+            startIcon={<AddCircleOutlineIcon />}
+            >
+              Add Activity 
             </Button>
-          </Link>
-          <Link to={`/opportunities/${id}/new-activity`}>
-            <Button size="small" color="primary">
-              Add Activity
-            </Button>
-          </Link>
         </CardActions>
       </Card>
     );
