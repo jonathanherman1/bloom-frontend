0f64e12be8622d5297fe79b43604b8e8f9c5e99e
shu-hu <shunuan.hu69@qmail.cuny.edu>
2021-08-25 12:11:48 -0400

    add contact detail page

---
+++ src/Components/ContactDetail/ContactDetail.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+
+import * as contactService from '../../services/contactService.js'
+
+const ContactDetail = (props) => {
+    const [contactDetail, setContactDetail] = useState('')
+    useEffect(() => {
+        (async () => {
+            const newContactDetail = await contactService.getContactById(props.match.params.contact_id)
+            setContactDetail(newContactDetail)
+        })()
+    }, []);
+    return (
+        <div>
+            <p>Contact Details</p>
+            <p>{contactDetail.name}</p>
+            <p>{contactDetail.phone}</p>
+            <p>{contactDetail.email}</p>
+            <p>{contactDetail.department}</p>
+            <p>{contactDetail.notes}</p>
+        </div>
+    );
+}
+
+export default withRouter(ContactDetail);
