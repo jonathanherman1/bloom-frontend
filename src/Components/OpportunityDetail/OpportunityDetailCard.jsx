3085a266ae973861736f795b5b058a8072287af8
teradaian <teradaian@gmail.com>
2021-08-25 14:31:17 -0700

    details page layout + root offset fix

--- src/Components/OpportunityDetail/OpportunityDetailCard.jsx
+++ src/Components/OpportunityDetail/OpportunityDetailCard.jsx
@@ -1,47 +1,40 @@
-import React from 'react';
-import Box from '@material-ui/core/Box'
-import Paper from '@material-ui/core/Paper';
-import Typography from '@material-ui/core/Typography'
-import FullWidthTab from './FullWidthTab/FullWidthTab'
-import Popover from '@material-ui/core/Popover';
-import classes from './OpportunityDetailCard.module.css'
-import Table from './Table/Table'
+import React from "react";
+import Box from "@material-ui/core/Box";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import FullWidthTab from "./FullWidthTab/FullWidthTab";
+import classes from "./OpportunityDetailCard.module.css";
+import Table from "./Table/Table";
 
-const OpportunityDetailCard = ({opportunityDetail}) => {
-    return (
-            <>
-            <Box className={classes.titleContainer} >
-                <Typography variant="h5">
-                    {opportunityDetail.name} at Company
-                </Typography>
-                <p>status</p>
-            </Box>
-            <Box 
-                component="fieldset" 
-                className={classes.detailsContainer}
-                mt={1}
-            >
-            <Box component="fieldset" mt={1} className={classes.container1}>
-                <Paper square>
-                    <FullWidthTab opportunityDetail={opportunityDetail}/>
-                </Paper>
-            </Box>
-
-            <Box component="fieldset" mt={1} className={classes.container2}>
-                <Box className={classes.container2left}>
-                    <p>pros</p>
-                </Box>
-                <Box className={classes.container2right}>
-                    <p>cons</p>
-                </Box>
-            </Box>
-            <Box component="fieldset" mt={1} className={classes.container3}>
-                <Table />
-            </Box>
-
-            </Box>
-            </>
+const OpportunityDetailCard = ({ opportunityDetail }) => {
+  return (
+    <>
+      <Box className={classes.titleContainer}>
+        <Typography variant="h5">
+          {opportunityDetail.name} at Company
+        </Typography>
+        <p>Status</p>
+      </Box>
+      <Box className={classes.detailsContainer} mt={1}>
+        <Box  mt={1} className={classes.container1}>
+          <Paper square>
+            <FullWidthTab opportunityDetail={opportunityDetail} />
+          </Paper>
+        </Box>
+        <Box  mt={1} className={classes.container2}>
+          <Box className={classes.container2left}>
+            <p>pros</p>
+          </Box>
+          <Box className={classes.container2right}>
+            <p>cons</p>
+          </Box>
+        </Box>
+        <Box  mt={1} className={classes.container3}>
+          <Table />
+        </Box>
+      </Box>
+    </>
   );
-}
- 
+};
+
 export default OpportunityDetailCard;
