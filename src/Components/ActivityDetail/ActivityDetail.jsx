057460eb5a3f53fe44998a623f805e1e91439376
HammerNTime <hammer.dylan@gmail.com>
2021-08-25 12:11:42 -0400

    get activity detail and index page stubbed up

---
+++ src/Components/ActivityDetail/ActivityDetail.jsx
@@ -0,0 +1,25 @@
+import React, { useState, useEffect } from 'react';
+import { withRouter } from 'react-router-dom';
+
+import * as activityService from '../../services/activityService.js'
+
+const ActivityDetail = (props) => {
+    const [activityDetail, setActivityDetail] = useState('')
+    console.log(props)
+    useEffect(() => {
+        (async () => {
+            const newActivityDetail = await activityService.getActivityById(props.match.params.act_id)
+            setActivityDetail(newActivityDetail)
+        })()
+    }, []);
+    return ( 
+    <div>
+        <p>details details details</p>
+        <p>{activityDetail.name}</p>
+        <p>{activityDetail.date}</p>
+        <p>{activityDetail.notes}</p>
+    </div> 
+    );
+}
+ 
+export default withRouter(ActivityDetail);
